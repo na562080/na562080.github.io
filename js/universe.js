351cf0e1b108dcfb0ee2b523a795ba57172219f4
na562080 <a13676414221@163.com>
2025-07-17 14:18:49 +0800

    Site updated: 2025-07-17 14:18:48

--- js/universe.js
+++ js/universe.js
@@ -3,9 +3,9 @@ function dark() {
     var n, e, i, h, t = .05,
         s = document.getElementById("universe"),
         o = !0,
-        a = "180,184,240",
-        r = "226,225,142",
-        d = "226,225,224",
+        a = "180,184,240",  // 明亮的星星颜色
+        r = "226,225,142",  // 一般星星的颜色
+        d = "226,225,224",  // 彗星颜色
         c = [];
 
     function f() {
@@ -22,15 +22,15 @@ function dark() {
 
     function y() {
         this.reset = function () {
-            this.giant = m(3), this.comet = !this.giant && !o && m(10), this.x = l(0, n - 10), this.y = l(0, e), this.r = l(1.1, 2.6), this.dx = l(t, 6 * t) + (this.comet + 1 - 1) * t * l(50, 120) + 2 * t, this.dy = -l(t, 6 * t) - (this.comet + 1 - 1) * t * l(50, 120), this.fadingOut = null, this.fadingIn = !0, this.opacity = 0, this.opacityTresh = l(.2, 1 - .4 * (this.comet + 1 - 1)), this.do = l(5e-4, .002) + .001 * (this.comet + 1 - 1)
+            this.giant = m(3), this.comet = !this.giant && !o && m(10), this.x = l(0, n - 10), this.y = l(0, e), this.r = l(1.1, 3.0), this.dx = l(t, 6 * t) + (this.comet + 1 - 1) * t * l(50, 120) + 2 * t, this.dy = -l(t, 6 * t) - (this.comet + 1 - 1) * t * l(50, 120), this.fadingOut = null, this.fadingIn = !0, this.opacity = 0, this.opacityTresh = l(.2, 1 - .4 * (this.comet + 1 - 1)), this.do = l(5e-4, .002) + .001 * (this.comet + 1 - 1)
         }, this.fadeIn = function () {
             this.fadingIn && (this.fadingIn = !(this.opacity > this.opacityTresh), this.opacity += this.do)
         }, this.fadeOut = function () {
             this.fadingOut && (this.fadingOut = !(this.opacity < 0), this.opacity -= this.do / 2, (this.x > n || this.y < 0) && (this.fadingOut = !1, this.reset()))
         }, this.draw = function () {
-            if (h.beginPath(), this.giant) h.fillStyle = "rgba(" + a + "," + this.opacity + ")", h.arc(this.x, this.y, 2, 0, 2 * Math.PI, !1);
+            if (h.beginPath(), this.giant) h.fillStyle = "rgba(" + a + "," + this.opacity + ")", h.arc(this.x, this.y, 3, 0, 2 * Math.PI, !1);  // 增大巨星的大小
             else if (this.comet) {
-                h.fillStyle = "rgba(" + d + "," + this.opacity + ")", h.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, !1);
+                h.fillStyle = "rgba(" + d + "," + this.opacity + ")", h.arc(this.x, this.y, 2, 0, 2 * Math.PI, !1);  // 彗星大小稍微加大
                 for (var t = 0; t < 30; t++) h.fillStyle = "rgba(" + d + "," + (this.opacity - this.opacity / 20 * t) + ")", h.rect(this.x - this.dx / 4 * t, this.y - this.dy / 4 * t - 2, 2, 2), h.fill()
             } else h.fillStyle = "rgba(" + r + "," + this.opacity + ")", h.rect(this.x, this.y, this.r, this.r);
             h.closePath(), h.fill()
@@ -48,6 +48,7 @@ function dark() {
     function l(t, i) {
         return Math.random() * (i - t) + t
     }
+
     f(), window.addEventListener("resize", f, !1),
         function () {
             h = s.getContext("2d");
